b378d5bdbe3fdd496228537b70bc5b107f318583
Ryan <rmanthey99@gmail.com>
2021-03-16 20:07:45 -1000

    implement show model with Foreign Key constrainsts

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -70,6 +70,13 @@ class Artist(db.Model):
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
+class Show(db.Model):
+  __tablename__ = 'Show'
+
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
