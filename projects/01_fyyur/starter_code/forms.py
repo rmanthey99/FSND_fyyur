170d38b532074a66ad2b09bcb9283b7c48855843
Ryan <rmanthey99@gmail.com>
2021-03-21 19:56:48 -1000

    added additional attributes to Venue in forms.py, works

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask_wtf import Form
+from sqlalchemy.sql.sqltypes import Boolean
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
+from wtforms.fields.core import BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL
 
 class ShowForm(Form):
@@ -115,6 +117,15 @@ class VenueForm(Form):
     )
     facebook_link = StringField(
         'facebook_link', validators=[URL()]
+    )
+    website = StringField(
+        'website', validators=[URL()]
+    )
+    seeking_talent = BooleanField(
+        'seeking_talent',
+    )
+    seeking_description = StringField(
+        'seeking_description'
     )
 
 class ArtistForm(Form):
