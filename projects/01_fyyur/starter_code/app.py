9684d7df13f14dc7f0ed0eeed8251e2fc27d36f3
Ryan <rmanthey99@gmail.com>
2021-04-05 19:58:12 -1000

    add recently added venue and artist to homepage, per reviewer

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -42,7 +42,9 @@ app.jinja_env.filters['datetime'] = format_datetime
 
 @app.route('/')
 def index():
-  return render_template('pages/home.html')
+  venues = Venue.query.order_by(Venue.id.desc()).limit(10).all()
+  artists = Artist.query.order_by(Artist.id.desc()).limit(10).all()
+  return render_template('pages/home.html', venues=venues, artists=artists)
 
 
 #  Venues
